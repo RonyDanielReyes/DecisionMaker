e79b7898bd4024ddffc9e87867f19049fb94df1a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-04 02:54:23 -0800

    add to database when submiting new poll

--- db/queries/polls.js
+++ db/queries/polls.js
@@ -10,14 +10,24 @@ const getPolls = () => {
       return data.rows;
     });
 };
-
+/**
+ * Get poll eith given id from the database.
+ * @param id poll id.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getPollById = (id) => {
+  return db.query('SELECT * FROM polls WHERE id = $1;', [id])
+    .then(data => {
+      return data.rows[0];
+    });
+};
 /**
  * Add a new poll to the database.
  * @param {{}} poll
  * @return {Promise<{}>} A promise to the user.
  */
 const addPoll = function(poll) {
-  return pool
+  return db
     .query(
       `
     INSERT INTO polls (creator_email, active, title, description, voter_link, admin_link)
@@ -36,5 +46,6 @@ const addPoll = function(poll) {
 
 module.exports = {
   getPolls,
+  getPollById,
   addPoll,
 };
