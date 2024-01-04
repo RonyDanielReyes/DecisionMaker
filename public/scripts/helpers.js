6c08e099ee3ffcb551bb0cd52fa52038719278db
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2024-01-04 04:09:26 -0800

    changing the routes

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,3 +1,4 @@
+
 //generates a random string made of lower or uppercase letters or digits 0-9 of given length
 const generateRandomString = function(length) {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
